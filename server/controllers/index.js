14a84de5ea490b3ac54b0aa3a2ad92e8bc31fcda
Milen Radkov <milenradkov@me.com>
2018-03-06 15:35:34 +0200

    Revert "basic ui"
    This reverts commit 95532b126bfa326c1b108cbc1caa7e9f4ca0b5da.

--- server/controllers/index.js
+++
@@ -1,9 +0,0 @@
-const home = require('./home-controller')
-const users = require('./users-controller')
-// const chats = require('./chats-controller')
-
-module.exports = {
-    home: home,
-    user: users,
-    // chats: chats
-}
